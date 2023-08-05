e312930c0ae3b5c3e9ad1a89cde636a5ac95bd01
emeka <emeka@pop-os.localdomain>
2023-08-05 08:12:55 +0100

    protected room&home page

--- src/context/withAuth.js
+++ src/context/withAuth.js
@@ -1,16 +1,19 @@
 // MyAuthenticatedComponent.js
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const withAuth = (WrappedComponent) => {
   const WithAuth = (props) => {
     // Authentication logic here
     const isAuthenticated = Boolean(Cookies.get("token"))
+    const navigate = useNavigate();
 
     if (isAuthenticated) {
       return <WrappedComponent {...props} />;
     } else {
-      return <Redirect to="/login" />;
+      navigate("/login");
+      return null;
     }
   };
 
